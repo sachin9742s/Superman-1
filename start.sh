862a9465a46644a31f4973265537a345f3a3e4b5
Jσєℓ Bσвαη [Oғғlιɴe] <95572329+JoelBobanOffline@users.noreply.github.com>
2022-04-30 14:22:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 echo "Cloning Repository"
-git clone https://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@github.com/JoelBobanOffline/Lauren_Remastered.git /Lauren
-fi
-cd /Lauren
+git clone https://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@github.com/JoelBobanOffline/Lauren_Remastered.git
+cd Lauren_Remastered
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
