895c57227dbe7774fa442d3e181939ce1a1d8e47
Jσєℓ Bσвαη [Oғғlιɴe] <95572329+JoelBobanOffline@users.noreply.github.com>
2022-04-30 08:18:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repository"
-git clone https://<pat>@github.com/JoelBobanOffline/Lauren_Remastered.git /Lauren
+git clone https://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@github.com/JoelBobanOffline/Lauren_Remastered.git /Lauren
 fi
 cd /Lauren
 pip3 install -U -r requirements.txt
