34762c43f4daa90a95b67fd84ea4d73c7ed6ce49
Jσєℓ Bσвαη [Oғғlιɴe] <95572329+JoelBobanOffline@users.noreply.github.com>
2022-05-01 09:36:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repository"
-git clone https://<pat>@github.com/JoelBobanOffline/Lauren_Remastered.git
-cd Lauren_Remastered
+git clone https://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@github.com/JoYaL-TG/Superman.git
+cd Superman
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
