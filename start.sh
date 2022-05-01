974c911d101c016e1ee921af02bb8adcb5811265
JoYaL-TG <98303967+JoYaL-TG@users.noreply.github.com>
2022-05-01 13:47:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
-echo "Cloning Repository"
-git clone https://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@github.com/JoYaL-TG/Superman.git
-cd Superman
+echo "Cloning Repo"
+  git clone ttps://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@github.com/JoYaL-TG/Superman.git
+fi
+cd /Superman
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
