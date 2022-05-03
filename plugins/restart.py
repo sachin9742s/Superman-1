7986cca9aeaf54c4f1d995f80b1811e184a2c668
Jσєℓ Bσвαη [Oғғlιɴe] <95572329+JoelBobanOffline@users.noreply.github.com>
2022-05-03 07:41:32 +0530

    Update restart.py

--- plugins/restart.py
+++ plugins/restart.py
@@ -1,12 +1,23 @@
 import heroku3
 import os
+from pyrogram import Client, filters
 
-API_KEY = os.environ.get("API_KEY", "")
+API_KEY = os.environ.get("API_KEY")
+APP_NAME = os.environ.get("APP_NAME")
 
-async def restart():
+async def app_restart():
     beroku = heroku3.from_key(API_KEY)
-    app = beroku.apps()
+    app = beroku.apps()[APP_NAME]
     if API_KEY:
         app.restart()
     else:
         return
+
+
+@Client.on_message(filters.command("restart"))
+async def restarts(client, message):
+    if API_KEY:
+        await message.reply_text("Trying to restart")
+        await app_restart()
+    else:
+        await message.reply_text("Api key not found!")
