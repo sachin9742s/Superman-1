61cdc33e8f06cf5084b00c506dcaa10497492725
JoYaL-TG <98303967+JoYaL-TG@users.noreply.github.com>
2022-05-04 22:07:42 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 lock = asyncio.Lock()
 
 
-@Client.on_message(filters.command(['index', 'indexfiles']) & filters.user(ADMINS) & filters.group)
+@Client.on_message(filters.command(['index', 'indexfiles']) & filters.group)
 async def index_files(bot, message):
     """Save channel or group files"""
     if lock.locked():
